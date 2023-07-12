04e6fc9423ec3f67aa5695e8da3d81537b275a2b
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-07-12 15:47:14 +0300

    тест

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-// import RegistrationScreen from './Screens/AuthPages/RegistrationScreen/RegistrationScreen';
-import LoginScreen from './Screens/AuthPages/LoginScreen/LoginScreen';
+import RegistrationScreen from './Screens/AuthPages/RegistrationScreen/RegistrationScreen';
+// import LoginScreen from './Screens/AuthPages/LoginScreen/LoginScreen';
 import PostsScreen from './Screens/PostsScreen/PostsScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-    {/* <RegistrationScreen></RegistrationScreen> */}
-    <LoginScreen></LoginScreen>
+    <RegistrationScreen></RegistrationScreen>
+    {/* <LoginScreen></LoginScreen> */}
     <PostsScreen></PostsScreen>
       <StatusBar style="auto" />
     </View>
